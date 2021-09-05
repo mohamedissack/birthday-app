59892f8079f59e1c45c44f193827d674570fab86
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-09-06 00:01:11 +0300

    create a java.js  file and update

---
+++ java.js
@@ -0,0 +1,30 @@
+var submission = function () {
+    var daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    var maleNames = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+    ];
+
+    var femaleNames = [
+        "Akosua",
+        " Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama"
+    ];
